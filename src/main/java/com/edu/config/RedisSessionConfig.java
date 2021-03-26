74b8937603baf248efb52850040f37e224aaede0
Lili <1432721789@qq.com>
2021-03-26 18:42:33 +0800

    no message

---
+++ src/main/java/com/edu/config/RedisSessionConfig.java
@@ -0,0 +1,9 @@
+package com.edu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds= 3600*24)
+public class RedisSessionConfig {
+}
