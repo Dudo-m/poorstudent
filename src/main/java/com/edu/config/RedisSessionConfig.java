ee5e52a8ad37967072a7fc3f919f4c5a5a38bf04
Lili <1432721789@qq.com>
2021-04-07 14:49:54 +0800

    update .gitignore

--- src/main/java/com/edu/config/RedisSessionConfig.java
+++
@@ -1,9 +0,0 @@
-package com.edu.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds= 3600*24)
-public class RedisSessionConfig {
-}
--- src/main/java/com/edu/entity/BaseEntity.java
+++
@@ -1,6 +0,0 @@
-package com.edu.entity;
-import java.io.Serializable;
-
-
-public abstract class BaseEntity implements Serializable {
-}
